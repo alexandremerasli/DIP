821939545a759c10f2b1bb2b9641cb5f2096329b
alexandremerasli <meraslia@pp-irs1-207558.localdomain>
2021-09-21 15:59:56 +0200

    post reconstruction results in tensorboard

--- block_2_test_post_reconstruction.py
+++ block_2_test_post_reconstruction.py
@@ -21,6 +21,7 @@ import os
 import sys
 from pathlib import Path
 import warnings
+from datetime import datetime
 
 # Math
 import numpy as np
@@ -32,6 +33,7 @@ import torch.nn as nn
 import torch.optim as optim
 import pytorch_lightning as pl
 from torchsummary import summary
+from torch.utils.tensorboard import SummaryWriter
 
 # Local files to import
 from models.ConvNet3D_real_lightning import *
@@ -49,14 +51,14 @@ config = {
     "sub_iter_DIP" : 200,
     "rho" : 0.003,
     "opti_DIP" : 'Adam',
-    "mlem_subsets" : False,
+    "mlem_subsets" : None, # None means we are in post reconstruction mode
     "d_DD" : 3,
     "k_DD" : 32
 }
 
 max_iter = 1 # Outer iterations
 test = 24 # Label of the experiment
-net = 'DD' # Network architecture
+net = 'DIP' # Network architecture
 processing_unit = 'CPU' # Processing unit (CPU or GPU)
 finetuning = 'False' # Finetuning (with best model or last saved model for checkpoint) or not for the DIP optimizations
 # PETImage_shape_str = sys.argv[6] # PET input dimensions (string, tF)
@@ -80,10 +82,31 @@ PETImage_shape = input_dim_str_to_list(PETImage_shape_str)
 
 ## Loading RAW stack of images
 # Loading DIP input (we do not have CT-map, so random image created in block 1)
-image_net_input_torch = torch.load(subroot + 'Data/image_net_input_torch.pt')# DO NOT CREATE RANDOM INPUT IN BLOCK 2 !!! ONLY AT THE BEGINNING, IN BLOCK 1. JUST LOAD IT FROM BLOCK 1
+# Creating random image input for DIP while we do not have CT, but need to be removed after
+create_random_input(net,PETImage_shape,config) # to be removed when CT will be used instead of random input. Here we CAN create random input as this script is only run once
+image_net_input = load_input(net,PETImage_shape,config) # Normalization of DIP input. DO NOT CREATE RANDOM INPUT IN BLOCK 2 !!! ONLY AT THE BEGINNING, IN BLOCK 1
+# image_net_input_norm, maxe_input = norm_imag(image_net_input) # Normalization of DIP input
+image_net_input_norm,mean_im,std_im = stand_imag(image_net_input) # Standardization of DIP input
+# DIP input image, numpy --> torch
+image_net_input_torch = torch.Tensor(image_net_input_norm)
+# Adding dimensions to fit network architecture
+if (net == 'DIP' or net == 'DIP_VAE'):
+    image_net_input_torch = image_net_input_torch.view(1,1,PETImage_shape[0],PETImage_shape[1]) # For DIP
+else:
+    if (net == 'DD'):
+        input_size_DD = int(PETImage_shape[0] / (2**config["d_DD"])) # if original Deep Decoder (i.e. only with decoder part)
+        image_net_input_torch = image_net_input_torch.view(1,config["k_DD"],input_size_DD,input_size_DD) # For Deep Decoder, if original Deep Decoder (i.e. only with decoder part)
+    elif (net == 'DD_AE'):
+        input_size_DD = PETImage_shape[0] # if auto encoder based on Deep Decoder
+        image_net_input_torch = image_net_input_torch.view(1,1,input_size_DD,input_size_DD) # For Deep Decoder, if auto encoder based on Deep Decoder
+torch.save(image_net_input_torch,subroot + 'Data/image_net_input_torch.pt')
+
+image_net_input_torch = torch.load(subroot + 'Data/image_net_input_torch.pt')# Here we CAN create random input as this script is only run once
 
 # Loading DIP x_label (corrupted image) from block1
 image_corrupt = fijii_np(subroot+'Comparaison/MLEM/MLEM_converge.img',shape=(PETImage_shape))
+#image_corrupt = fijii_np(subroot+'Comparaison/MLEM/MLEM_it2.img',shape=(PETImage_shape))
+
 # Normalization of x_label image
 # image_corrupt_norm_scale, maxe = norm_imag(image_corrupt) # Normalization of x_label image
 image_corrupt_norm,mean_label,std_label= stand_imag(image_corrupt) # Standardization of x_label image
@@ -105,12 +128,12 @@ def train_process(config, finetuning, processing_unit, sub_iter_DIP, max_iter, i
     # Choose network architecture as model
     model, model_class = choose_net(net, config)
 
-    checkpoint_simple_path = subroot+'Block2/checkpoint/'
-    checkpoint_simple_path_exp = subroot+'Block2/checkpoint/'+format(test)  + '/' + suffix + '/'
+    checkpoint_simple_path = 'runs/' # To log loss in tensorboard thanks to Logger
+    checkpoint_simple_path_exp = '' # We do not need its value in this script
     
     # Start training
     print('Starting optimization, iteration',max_iter)
-    trainer = create_pl_trainer(finetuning, processing_unit, sub_iter_DIP, checkpoint_simple_path, test, checkpoint_simple_path_exp)
+    trainer = create_pl_trainer(finetuning, processing_unit, sub_iter_DIP, checkpoint_simple_path, test, checkpoint_simple_path_exp,name=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
     trainer.fit(model, train_dataloader)
 
@@ -121,14 +144,19 @@ model = train_process(config, finetuning, processing_unit, sub_iter_DIP, max_ite
 """
 Saving variables and model
 """
-if (net == 'DIP_VAE'):
-    out, mu, logvar, z = model(image_net_input_torch)
-else:
-    out = model(image_net_input_torch)
 
-# Destandardize like at the beginning
-out_destand = destand_imag(out, mean_label, std_label)
-# Saving image output
-save_img(out_destand, subroot+'Block2/out_cnn/' + format(test) + '/out_DIP_post_reco_epoch=' + format(sub_iter_DIP) + '.img')
+writer = model.logger.experiment # Assess to new variable, otherwise error : weakly-referenced object ...
+for epoch in range(0,sub_iter_DIP,sub_iter_DIP//10):
+    # Load saved (STANDARDIZED) images
+    net_outputs_path = subroot+'Block2/out_cnn/' + format(test) + '/out_' + net + '_post_reco_epoch=' + format(epoch) + '.img'
+    out = fijii_np(net_outputs_path,shape=(PETImage_shape))
+    # Destandardize like at the beginning
+    out_destand = destand_numpy_imag(out, mean_label, std_label)
+    # Saving (now DESTANDARDIZED) image output
+    save_img(out_destand, net_outputs_path)
+    # Write images over epochs
+    write_image_tensorboard(writer,out_destand,"Image over epochs (" + net + "output)",epoch) # Showing all images with same contrast to compare them together
+    write_image_tensorboard(writer,out_destand,"Image over epochs (" + net + "output, FULL CONTRAST)",epoch,full_contrast=True) # Showing each image with contrast = 1
+    writer.close()
 
 print('Finish')
