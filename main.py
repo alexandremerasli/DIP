cd941819027c9e2a4422ac28200b6d3a45ec031b
Merasli <alexandre.merasli@outlook.fr>
2022-01-17 15:44:04 +0100

    config separed between hyperparameters to tune and parameters for computation with main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@
 ## Python libraries
 # Useful
 import os
-import argparse
-from functools import partial
 from ray import tune
 
 # Local files to import
@@ -17,12 +15,18 @@ from utils.utils_func import *
 from iNestedADMM import iNestedADMM
 from iPostReconstruction import iPostReconstruction
 
-# Configuration dictionnary for hyperparameters to tune
-#'''
-config = {
+# Configuration dictionnary for general parameters (not hyperparameters)
+fixed_config = {
     "image" : tune.grid_search(['image0']),
-    "net" : tune.grid_search(['DIP']), # Network to use (DIP,DD,DIP_VAE)
+    "net" : tune.grid_search(['DIP']), # Network to use (DIP,DD,DD_AE,DIP_VAE)
     "method" : tune.grid_search(['nested']),
+    "processing_unit" : tune.grid_search(['CPU']),
+    "max_iter" : tune.grid_search([10]),
+    "finetuning" : tune.grid_search(['last']),
+    "test" : tune.grid_search([24])
+}
+# Configuration dictionnary for hyperparameters to tune
+hyperparameters_config = {
     "rho" : tune.grid_search([0.0003]),
     # network hyperparameters
     "lr" : tune.grid_search([0.01]), # 0.01 for DIP, 0.001 for DD
@@ -35,34 +39,20 @@ config = {
     "k_DD" : tune.grid_search([32]),
     # ADMMLim hyperparameters
     "sub_iter_MAP" : tune.grid_search([10]), # Block 1 iterations (Sub-problem 1 - MAP) if mlem_sequence is False
-    "nb_iter_second_admm": tune.grid_search([10]), # Number of ADMM iterations (ADMM before NN)
+    "nb_iter_second_admm": tune.grid_search([10
+    ]), # Number of ADMM iterations (ADMM before NN)
     "mlem_sequence" : tune.grid_search([False]),
     "alpha" : tune.grid_search([0.005])
 }
-#'''
 
-## Arguments for linux command to launch script
-# Creating arguments
-parser = argparse.ArgumentParser(description='DIP + ADMM computation')
-parser.add_argument('--proc', type=str, dest='proc', help='processing unit (CPU, GPU or both)')
-parser.add_argument('--max_iter', type=int, dest='max_iter', help='number of outer iterations')
-parser.add_argument('--finetuning', type=str, dest='finetuning', help='finetuning or not for the DIP optimizations', nargs='?', const='False')
-
-# Retrieving arguments in this python script
-args = parser.parse_args()
-
-# For VS Code (without command line)
-if (args.proc is None): # Must check if all args are None
-    args.proc = 'CPU'
-    args.max_iter = 10 # Outer iterations
-    args.finetuning = 'last' # Finetuning or not for the DIP optimizations (block 2)
-
-
-
-
+# Merge 2 dictionaries
+split_config = {
+    "hyperparameters" : list(hyperparameters_config.keys())
+}
+config = {**fixed_config, **hyperparameters_config, **split_config}
 
 # Initializer images and variables
 root = os.getcwd()
-classReco = iNestedADMM(config,args,root)
-#classReco = iPostReconstruction(config,args,root)
-classReco.runRayTune(config,args,root)
+classReco = iNestedADMM(hyperparameters_config,root)
+#classReco = iPostReconstruction(config,root)
+classReco.runRayTune(config,root)
