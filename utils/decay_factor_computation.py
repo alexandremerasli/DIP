f90acf4d0785728dfa3ec9f8444731e92dcd7a8d
Merasli <alexandre.merasli@outlook.fr>
2023-08-16 15:06:57 +0200

    understanding ecat7 files normalization factors

--- utils/decay_factor_computation.py
+++ utils/decay_factor_computation.py
@@ -8,3 +8,13 @@ duration_acq = 1809
 DC = lambda_rad * duration_acq / (1-math.exp(-lambda_rad * duration_acq))
 
 print("decay factor = ", DC)
+
+
+norm_factor_cylinder = 1.85471e7
+ecat7_reco_factor_cylinder = 1.91741e7
+norm_factor_brain = 1.85078e7
+ecat7_reco_factor_brain = 1.91335e7
+
+print("ratio factor cylinder = ",norm_factor_cylinder / ecat7_reco_factor_cylinder)
+print("ratio factor brain = ",norm_factor_brain / ecat7_reco_factor_brain)
+print("reversed ratio factor brain = ",ecat7_reco_factor_brain / norm_factor_brain)
